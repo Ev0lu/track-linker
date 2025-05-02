3406bf10d795528cae16cdd373537f262e2858a5
Max <74422818+Ev0lu@users.noreply.github.com>
2025-05-02 09:52:04 +0300

    Delete src directory

--- src/app/pages/search/model/index.ts
+++
@@ -1,9 +0,0 @@
-export async function searchSong(url: string) {
-  const response = await fetch("https://tracklinker.ru/track", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "Accept": "application/json" },
-    body: JSON.stringify({ url }),
-  });
-  if (!response.ok) throw new Error("Трек не найден");
-  return response.json();
-}
--- src/app/pages/song/model/index.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchSong(id: any) {
-    const response = await fetch(`https://tracklinker.ru/track/${id}`);
-    if (!response.ok) throw new Error("Failed to fetch song");
-    return response.json();
-  }
